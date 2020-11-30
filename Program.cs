db934fec70aa9ee5c4b4380f1f0c58a18e179204
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2020-11-29 22:09:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lógica_de_Programação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*aqui vai o código*/
+
+            // Console.WriteLine("Digite o seu nome:");
+            // string nomeCompleto = Console.ReadLine();
+
+            // Console.WriteLine("Bem vindo "+nomeCompleto);
+
+        int valorUm; 
+        int valorDois; 
+        
+
+        // Entrada de dados
+        Console.WriteLine("soma de dois números");
+        Console.WriteLine("-----------");
+        Console.WriteLine("Digite o primeiro valor");
+        valorUm = int.Parse(Console.ReadLine());
+        Console.WriteLine("digite o segundo valor");
+        valorDois = int.Parse(Console.ReadLine());
+
+        // Processamento
+        int soma = valorUm + valorDois;
+
+        // Exibir
+        Console.WriteLine("a soma de valor 1 + valor 2 = "+ soma);
+
+
+
+        }
+    }
+}
